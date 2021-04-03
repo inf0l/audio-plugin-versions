cf6cb0410cb1bc2d1d9ec352b5f00fb50a58cef1
Linus Bergman <linusbergman@pm.me>
2021-04-03 21:25:28 +0300

    Update to include CFBundleIdentifier and search subfolders recursively.

--- plugver.py
+++ plugver.py
@@ -26,34 +26,53 @@ print("""
       any previous versions of 'pluginlist.txt'.
 
       """)
-FILENAME = input(
+filename = input(
     'Enter a filename (leave blank for default "pluginlist.txt"): ')
 
 
-def findVersion(format: str, plugDir: Path, FILENAME='pluginlist.txt'):
-    with open(FILENAME, 'a') as f:
-        for pluginPath in list(plugDir.glob('*.*')):
+def findVersion(format: str,
+                plugDir: Path,
+                extension,
+                filename='pluginlist.txt'):
+    with open(filename, 'a') as f:
+        for pluginPath in list(plugDir.glob(f'**/*.{extension}')):
+            if 'Pace_Eden' in str(pluginPath):
+                continue
             plugin = str(pluginPath.stem)
             try:
                 tree = ET.parse(f'{str(pluginPath)}/Contents/Info.plist')
                 root = tree.getroot()
                 elements = [elem.text for elem in root.iter()]
-                tag = 'CFBundleShortVersionString'
-                if tag in elements:
-                    index = elements.index(tag)
-                    version = elements[index + 1]
+                version_tag = 'CFBundleShortVersionString'
+                manufacturer_tag = 'CFBundleIdentifier'
+                if version_tag in elements:
+                    version_index = elements.index(version_tag)
+                    version = elements[version_index + 1]
                     if version.startswith('0x'):
                         version = version.split()[-1]
+                if manufacturer_tag in elements:
+                    manufacturer_index = elements.index(manufacturer_tag)
+                    manufacturer = elements[manufacturer_index + 1]
+                else:
+                    manufacturer_tag = ''
             except:
-                version = 'Info.plist not found'
+                version = 'not found'
             finally:
-                if plugin.startswith('.'):
+                if plugin.startswith('.') or version == 'not found':
                     continue
-                print(f'{format:15} {plugin:40} {version}', file=f)
+                print(
+                    f'{format:15} {plugin:40} {version:40} {manufacturer:>60}',
+                    file=f)
 
 
-findVersion('AAX', AAXDIR, FILENAME)
-findVersion('AAX (Unused)', AAX_UNUSEDDIR, FILENAME)
-findVersion('VST', VSTDIR, FILENAME)
-findVersion('VST3', VST3DIR, FILENAME)
-findVersion('AU', AUDIR, FILENAME)
+findVersion('AAX', AAXDIR, 'aaxplugin', filename) if filename else findVersion(
+    'AAX', AAXDIR, 'aaxplugin')
+findVersion('AAX (Unused)', AAX_UNUSEDDIR,
+            'aaxplugin', filename) if filename else findVersion(
+                'AAX (Unused)', AAX_UNUSEDDIR, 'aaxplugin')
+findVersion('VST', VSTDIR, 'vst', filename) if filename else findVersion(
+    'VST', VSTDIR, 'vst')
+findVersion('VST3', VST3DIR, 'vst3', filename) if filename else findVersion(
+    'VST3', VST3DIR, 'vst3')
+findVersion('AU', AUDIR, 'component', filename) if filename else findVersion(
+    'AU', AUDIR, 'component')
